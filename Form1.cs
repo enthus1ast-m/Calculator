8286cc68d972cefd5f4d0c8c55082670d2ccc7f4
M1han <65967618+enthus1ast-m@users.noreply.github.com>
2022-04-13 22:38:50 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Method()
+        {
+
+            int n = 1;
+                
+                label1.Text += (n).ToString();
+            
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            Method();
+            
+        }
+    
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        
+    }
+}
